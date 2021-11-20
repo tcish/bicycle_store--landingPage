48a2ce5e7459807daaa66d136b17e87b897bc02b
tcish <khaladkhan250@gmail.com>
2021-11-20 12:08:27 +0600

    added toggle menu

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const cycle3El = document.querySelector("#cycle3")
 const cycleNameEl = document.querySelector("#cycleName")
 const cycleImgEl = document.querySelector("#cycleImg")
 const cyclePriceEl = document.querySelector("#cyclePrice")
+const toggleEl = document.querySelector("#toggleBtn")
+const navbarEl = document.querySelector(".navbar")
+const leftSideEl = document.querySelector(".left__side")
+const toggleCloseEl = document.querySelector("#toggleClose")
 
 function cycleDetails(cycle, name, image, price) {
   cycle.addEventListener("click", () => {
@@ -17,3 +21,13 @@ function cycleDetails(cycle, name, image, price) {
 cycleDetails(cycle1El, "eFB01", "img/cycle1.png", "$1250")
 cycleDetails(cycle2El, "eR02 Apex", "img/cycle2.png", "$1020")
 cycleDetails(cycle3El, "eM02 FS Performance 29", "img/cycle3.png", "$1155")
+
+toggleEl.addEventListener("click", () => {
+  navbarEl.classList.add("active")
+  leftSideEl.classList.add("active")
+})
+
+toggleCloseEl.addEventListener("click", () => {
+  navbarEl.classList.remove("active")
+  leftSideEl.classList.remove("active")
+})
